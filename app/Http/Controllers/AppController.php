559b1792b1ae8ada97cd27eb098c1fe894528163
diogogoncalves <dmgdark95@gmail.com>
2025-10-31 21:27:56 +0000

    Add Auth Web

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Gate;
 
 class AppController
 {
     protected function allowedAction($permission)
     {
-        if (auth()->user()->hasPermission($permission)) {
-            throw new AuthenticationException('This action is unauthorized');
+        if (Gate::denies('authorization', $permission)) {
+            throw new AuthorizationException('This action is unauthorized');
         }
     }
 }
