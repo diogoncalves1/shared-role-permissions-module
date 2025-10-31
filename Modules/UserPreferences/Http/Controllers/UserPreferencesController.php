12e556893df66aa0a1b1a126624d616ce1716928
diogogoncalves <dmgdark95@gmail.com>
2025-10-31 21:28:49 +0000

    Add User Preferences Module

---
+++ Modules/UserPreferences/Http/Controllers/UserPreferencesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\UserPreferences\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserPreferencesController extends Controller
+{
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id) {}
+}
